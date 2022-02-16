95df1c52353fd2f9e91237993fb9f5710e57e62e
Ducorits <80724765+Ducorits@users.noreply.github.com>
2022-02-16 21:51:28 +0100

    First empty mod setup

---
+++ src/main/java/net/ducorits/ip/edit/mixin/IpEditMixin.java
@@ -0,0 +1,16 @@
+package net.ducorits.ip.edit.mixin;
+
+import net.ducorits.ip.edit.IpEdit;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class IpEditMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		IpEdit.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
