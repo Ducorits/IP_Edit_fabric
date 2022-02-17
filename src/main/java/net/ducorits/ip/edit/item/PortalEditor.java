59ac1530fe74e2af551b5a968367a4321960a214
Ducorits <80724765+Ducorits@users.noreply.github.com>
2022-02-17 18:02:19 +0100

    Got it working with immersive portals.

--- src/main/java/net/ducorits/ip/edit/item/PortalEditor.java
+++ src/main/java/net/ducorits/ip/edit/item/PortalEditor.java
@@ -27,21 +27,29 @@ public class PortalEditor extends Item {
 //        FireballEntity fireballEntity = new FireballEntity(world, user, f, g, h, 1);
 //        fireballEntity.setPosition(user.getX() + vec3d.x * 2.0, user.getBodyY(.9) + vec3d.y * 2.0, fireballEntity.getZ() + vec3d.z * 2.0);
 //        world.spawnEntity(fireballEntity);
+
+        Vec3d vec3d = user.getRotationVec(1.0f);
+        double x = user.getX() + (vec3d.x * 2.0);
+//        double g = user.getBodyY(0.5);
+        double y = user.getY() + (vec3d.y * 2.0);
+        double z = user.getZ() + (vec3d.z * 2.0);
+        Portal portal = Portal.entityType.create(world);
+        portal.setOriginPos(new Vec3d(x, y, z));
+        portal.setDestinationDimension(World.NETHER);
+        portal.setDestination(new Vec3d(z / 8, (y + 88) / 3.3, x / 8));
+        portal.setOrientationAndSize(
+                new Vec3d(1, 0, 0), // axisW
+                new Vec3d(0, 1, 0), // axisH
+                4, // width
+                4 // height
+        );
+        portal.world.spawnEntity(portal);
         if (!world.isClient) {
-            user.sendMessage(new LiteralText("Staff Functions here"), false);
+            user.sendMessage(new LiteralText("x:" + x + " y:" + y + " z:" + z), false);
+            user.sendMessage(new LiteralText("AXISW vecx:" + vec3d.z + " vecy:" + vec3d.y + " vecz:" + vec3d.x * -1), false);
+            user.sendMessage(new LiteralText("AXISH vecx:" + vec3d.y + " vecy:" + vec3d.x * -1 + " vecz:" + vec3d.z), false);
+            user.sendMessage(new LiteralText("Vector vecx:" + vec3d.x + " vecy:" + vec3d.y + " vecz:" + vec3d.z), false);
         }
-//        Portal portal = Portal.entityType.create(serverWorld);
-//        portal.setOriginPos(new Vec3d(0, 70, 0));
-//        portal.setDestinationDimension(World.NETHER);
-//        portal.setDestination(new Vec3d(100, 70, 100));
-//        portal.setOrientationAndSize(
-//                new Vec3d(1, 0, 0), // axisW
-//                new Vec3d(0, 1, 0), // axisH
-//                4, // width
-//                4 // height
-//        );
-//        portal.world.spawnEntity(portal);
-//
 
         return TypedActionResult.success(itemStack, true);
     }
